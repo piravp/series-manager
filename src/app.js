7ffcdf317f987e48f0d96593d40d994d6e78364f
Piraveen <pira.rajan@gmail.com>
2018-04-05 01:00:35 +0200

    Initialize repository

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainPage from './components/MainPage';
+
+import './styles/styles.scss';
+
+
+ReactDOM.render(<MainPage />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// Node command to import 'path' module
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
+
+// style-loader:    Injects a <style> tag into the DOM  
+// css-loader:      Teach webpack how to take a CSS file and turn it into a JavaScript representation
+// sass-loader:     Works just like the babel-loader works for jsx. This module is going to convert scss/sass to regular
+//                  css. It's using the node-sass module behind the scenes (just like babel-loader uses babel-core).
