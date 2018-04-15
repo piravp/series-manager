7455f677cce1be691f536f0765d919d87af93e7f
Piraveen <pira.rajan@gmail.com>
2018-04-15 04:34:12 +0200

    Improve filter toolbar in HomePage

--- src/components/Home/HomeSeriesFilter.js
+++ src/components/Home/HomeSeriesFilter.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Input, Button, Select } from 'antd'; 
+const Search = Input.Search;
+const Option = Select.Option;
+
 
 import { 
     setTextFilter, 
@@ -14,54 +18,49 @@ import { removeAllShows } from '../../actions/series';
 const HomeSeriesFilter = (props) => (
     <div className="homeSeriesFilterContainer">
 
-        <input autoFocus 
-               type='text'
-               name='filter' 
-               value={props.filters.text} onChange={(e) => {
-                        props.dispatch(setTextFilter({ text: e.target.value }))
-                    }}
-                placeholder="Search through your list"
-        />
-
-        <select disabled={props.seriesListItems === 0}
-                value={props.filters.sortBy}
-                onChange={(e) => {
-                    switch(e.target.value){
-                        case 'name_ascending':
-                            return props.dispatch(sortByNameAscending());
-                        case 'name_descending':
-                            return props.dispatch(sortByNameDescending());
-                        case 'date_ascending':
-                            return props.dispatch(sortByDateAscending());
-                        case 'date_descending':
-                            return props.dispatch(sortByDateDescending());
-                        case 'rating_ascending':
-                            return props.dispatch(sortByRatingAscending());
-                        case 'rating_descending':
-                            return props.dispatch(sortByRatingDescending());
-                    }
-
-
-                }}
+        <div className="homeSeriesFilterChildContainer">
+            <Select defaultValue="name_ascending" style={{ width: 190 }} disabled={props.seriesListItems === 0} onChange={value => {
+                switch(value){
+                    case 'name_ascending':
+                        return props.dispatch(sortByNameAscending());
+                    case 'name_descending':
+                        return props.dispatch(sortByNameDescending());
+                    case 'date_ascending':
+                        return props.dispatch(sortByDateAscending());
+                    case 'date_descending':
+                        return props.dispatch(sortByDateDescending());
+                    case 'rating_ascending':
+                        return props.dispatch(sortByRatingAscending());
+                    case 'rating_descending':
+                        return props.dispatch(sortByRatingDescending());
+                }
+            }}>
+                <Option value="name_ascending">Name - ascending</Option>
+                <Option value="name_descending">Name - descending</Option>
+                <Option value="date_ascending">Aired date - ascending</Option>
+                <Option value="date_descending">Aired date - descending</Option>
+                <Option value="rating_ascending">Rating - ascending</Option>
+                <Option value="rating_descending">Rating - descending</Option>
+            </Select>
         
-        >
-            <option value="name_ascending">Name - ascending</option>
-            <option value="name_descending">Name - descending</option>
-            <option value="date_ascending">Aired date - ascending</option>
-            <option value="date_descending">Aired date - descending</option>
-            <option value="rating_ascending">Rating - ascending</option>
-            <option value="rating_descending">Rating - descending</option>
-        </select>
+            <Search
+                className="searchbarInHome"
+                placeholder="Search through your list"
+                onChange={(e) => {
+                    props.dispatch(setTextFilter({ text: e.target.value }))
+                }}
+                autoFocus/>
 
-        <button onClick={(e) => {
+
+                <Button onClick={(e) => {
                     props.dispatch(removeAllShows())
                 }}
                 disabled={props.seriesListItems === 0}
-        >
-            Remove all
-        </button>
-
-
+                type="danger"
+                >
+                    Remove all
+                </Button>
+        </div>
     </div>
 );
 
